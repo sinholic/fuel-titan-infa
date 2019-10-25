10386174d1c5d9b3ab43e8d473f27b7f968003c0
Muhammad Iqbal <iqbalgooner33@gmail.com>
2019-10-25 07:01:08 +0700

    Menambahkan equipmentmodel.php

--- app/EquipmentModel.php
+++ app/EquipmentModel.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class EquipmentModel extends Model
 {
-    protected $table = "equipment";
-    protected $fillable = ['equipment_name', 'equipment_number', 'equipment_category', 'location', 'fuel_capacity', 'machine_hours', 'last_machine_hours', 'last_maintenance', 'pic'];
-
+    protected $table = "equipment_unitdata";
+    protected $fillable = ['equipment_number', 'equipment_category', 'location', 'fuel_capacity', 'machine_hours', 'last_machine_hours', 'std_consumption', 'last_ending_stock', 'add_fuel', 'last_maintenance', 'pic'];
     public $timestamps = false;
 }
