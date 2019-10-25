8f75d5de79e3e6e17eecd0da485855bd88066d06
Muhammad Iqbal <iqbalgooner33@gmail.com>
2019-10-25 07:05:47 +0700

    Menambahkan form tambah_equipment.blade.php

--- resources/views/Equipment/tambah_equipment.blade.php
+++ resources/views/Equipment/tambah_equipment.blade.php
@@ -3,85 +3,94 @@
 @section('content')
 
 <div class="row">
-	<div class="col-md-12">
-		<form action="/equipment/create" method="POST">
+    <div class="col-md-12">
+        <form action="/equipment/create" method="POST">
 
-			@csrf
+            @csrf
 
-			<div class="card" style="border-top: 3px solid #9C5C22">
-				<div class="card-header">
-					<h3 class="card-title">Tambah</h3>
-				</div>
+            <div class="card" style="border-top: 3px solid #9C5C22">
+                <div class="card-header">
+                    <h3 class="card-title">Tambah Equipment & Unit Data</h3>
+                </div>
 
-				<div class="card-body">
+                <div class="card-body">
 
-					@if($errors->any())
-					<div class="alert alert-danger">
-						{{implode('', $errors->all(':message'))}}
-					</div>
-					@endif
+                    @if($errors->any())
+                    <div class="alert alert-danger">
+                        {{implode('', $errors->all(':message'))}}
+                    </div>
+                    @endif
 
-					<div class="form-group">
-						<label>Nama Equipment</label>
-						<input type="text" name="equipment_name" placeholder="" class="form-control" required autofocus>
+                    <div class="form-group">
+                        <label>Equipment Number</label>
+                        <input type="text " name="equipment_number" placeholder="" class="form-control" required autofocus>
                     </div>
 
                     <div class="form-group">
-                        <label for="">Equipment Number</label>
-                        <input type="text" name="equipment_number" placeholder="" class="form-control" required autofocus>
-                    </div>
-                    
-                    <div class="form-group">
-						<label>Equipment Category</label>
-						<input type="text" name="equipment_category" placeholder="" class="form-control" required autofocus>
-                    </div>
-                    
-                    <div class="form-group">
-						<label>Location</label>
-						<input type="text" name="location" placeholder="" class="form-control" required autofocus>
-                    </div>
-                    
-                    <div class="form-group">
-						<label>Fuel Capacity</label>
-						<input type="text" name="fuel_capacity" placeholder="" class="form-control" required autocomplete="">
-                    </div>
-                    
-                    <div class="form-group">
-						<label>Machine Hours</label>
-						<input type="text" name="machine_hours" placeholder="" class="form-control" required autocomplete>
+                        <label for="">Equipment Category</label>
+                        <input type="text" name="equipment_category" placeholder="" class="form-control" required autofocus>
                     </div>
 
                     <div class="form-group">
-                        <label for="">Last Machine Hours</label>
-                        <input type="text" class="form-control" name="last_machine_hours">
+                        <label>Location</label>
+                        <input type="text" name="location" placeholder="" class="form-control" required autofocus>
                     </div>
 
                     <div class="form-group">
-                        <label for="">Last Maintenance</label>
-                        <input type="text" class="form-control" name="last_maintenance">
+                        <label>Fuel Capacity</label>
+                        <input type="text" name="fuel_capacity" placeholder="" class="form-control" required autofocus>
                     </div>
 
                     <div class="form-group">
-                        <label for="">PIC</label>
-                        <input type="text" class="form-control" name="pic">
+                        <label>Machine Hours</label>
+                        <input type="text" name="machine_hours" placeholder="" class="form-control" required autocomplete="">
                     </div>
-                     
 
-				</div>
+                    <div class="form-group">
+                        <label>Last Machine Hours</label>
+                        <input type="text" name="last_machine_hours" placeholder="" class="form-control" required autofocus>
+                    </div>
 
-				<div class="card-footer">
+                    <div class="form-group">
+                        <label>Std Consumption</label>
+                        <input type="text" name="std_consumption" placeholder="" class="form-control" required autocomplete="">
+                    </div>
 
-					<a href="/equipment" class="btn btn-default">Back</a>
+                    <div class="form-group">
+                        <label>Last Ending Stock</label>
+                        <input type="text" name="last_ending_stock" placeholder="" class="form-control" required autofocus>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Add Fuel</label>
+                        <input type="text" name="add_fuel" placeholder="" class="form-control" required autocomplete="">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Last Maintenance</label>
+                        <input type="date" name="last_maintenance" placeholder="" class="form-control" required autocomplete="">
+                    </div>
+
+                    <div class="form-group">
+                        <label>PIC</label>
+                        <input type="text" name="pic" placeholder="" class="form-control" required autocomplete="">
+                    </div>
+
+                </div>
+
+                <div class="card-footer">
+
+                    <a href="/equipment" class="btn btn-default">Back</a>
                     &nbsp;&nbsp;
-					<input type="submit" value="Add Data" class="pull-right btn btn-primary">
+                    <input type="submit" value="Add Data" class="pull-right btn btn-primary">
 
-				</div>
+                </div>
 
-			</div>
+            </div>
 
-		</form>
+        </form>
 
-	</div>
+    </div>
 </div>
 
 
