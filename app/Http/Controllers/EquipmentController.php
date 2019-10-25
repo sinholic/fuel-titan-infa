1e0d6b0623f8414624035c9f3d35accead801b93
Muhammad Iqbal <iqbalgooner33@gmail.com>
2019-10-25 07:02:58 +0700

    Menambahkan EquipmentController.php

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -2,13 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\EquipmentModel;
 use Illuminate\Http\Request;
-use Maatwebsite\Excel\Facades\Excel;
-use App\Http\Controllers\Controller;
-use App\Exports\EquipmentExport;
-use App\Imports\EquipmentImport;
-
+use \App\EquipmentModel;
 
 class EquipmentController extends Controller
 {
@@ -18,26 +13,15 @@ class EquipmentController extends Controller
         return view('Equipment.equipment', ['equipment' => $equipment]);
     }
 
-    public function tampiladd()
+    public function tambah()
     {
         return view('Equipment.tambah_equipment');
     }
 
     public function create(Request $request)
     {
-        $this->validate($request, [
-            'equipment_name' => 'required',
-            'equipment_number' => 'required',
-            'equipment_category' => 'required',
-            'location' => 'required',
-            'fuel_capacity' => 'required',
-            'machine_hours' => 'required',
-            'last_machine_hours' => 'required',
-            'last_maintenance' => 'required',
-            'pic' => 'required',
-        ]);
         EquipmentModel::create($request->all());
-        return redirect('/equipment')->with('sukses', 'Data Berhasil Di Input!');
+        return redirect('/equipment')->with('sukses', 'Data Berhasil di Input!');
     }
 
     public function edit($id)
@@ -50,45 +34,6 @@ class EquipmentController extends Controller
     {
         $equipment = EquipmentModel::find($id);
         $equipment->update($request->all());
-        return redirect('/equipment')->with('sukses', 'Data Berhasil Di Update!');
-    }
-
-    public function delete($id)
-    {
-        $equipment = EquipmentModel::find($id);
-        $equipment->delete($equipment);
-        return redirect('/equipment')->with('sukses', 'Data berhasil dihapus!');
-    }
-
-
-    public function export_excel()
-    {
-        return Excel::download(new EquipmentExport, 'master_data_equipment.xlsx');
-    }
-
-    public function import_excel(Request $request)
-    {
-        //validasi
-        $this->validate($request, [
-            'file' => 'required|mimes:csv,xls,xlsx'
-        ]);
-
-        //menangkap file excel
-        $file = $request->file('file');
-
-        //membuat nama file unik
-        $nama_file = rand() . $file->getClientOriginalName();
-
-        //upload ke folder file_station di dalam folder public
-        $file->move('file_equipment', $nama_file);
-
-        //import data
-        Excel::import(new EquipmentImport, public_path('file_equipment/' . $nama_file));
-
-        //notifikasi dengan session
-        Session::flash('sukses', 'Data Master Station Berhasil Di Import!');
-
-        //alihkan kembali
-        return redirect('/equipment');
+        return redirect('/equipment')->with('sukses', 'Data berhasil di Update!');
     }
 }
