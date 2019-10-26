d27b864d0b3c0ae96718063e6391732f183b955b
Muhammad Iqbal <iqbalgooner33@gmail.com>
2019-10-26 11:17:06 +0700

    Membuat migrations table fix_station

---
+++ database/migrations/2019_10_25_230150_create_fix_station_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFixStationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fix_station', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name_station');
+            $table->string('address');
+            $table->string('nama_lokasi');
+            $table->string('koordinat_gps');
+            $table->string('tank_number');
+            $table->string('fuel_capacity');
+            $table->string('fuel_assignment');
+            $table->string('last_refuel');
+            $table->string('ending_stock_date');
+            $table->string('ending_stock_quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fix_station');
+    }
+}
