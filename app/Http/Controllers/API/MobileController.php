fd053602874cf4d6c463a77ab61f9a2020646e70
Muhammad Iqbal <iqbalgooner33@gmail.com>
2019-10-26 13:51:25 +0700

    Membuat API MobileController.php

---
+++ app/Http/Controllers/API/MobileController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\MobileModel;
+
+class MobileController extends Controller
+{
+    public function mobile()
+    {
+        $mobile = MobileModel::all();
+        return response()->json(['data' => $mobile]);
+    }
+
+    public function tambah()
+    {
+        return view('Mobile Station.tambah_mobile');
+    }
+
+    public function create(Request $request)
+    {
+        MobileModel::create($request->all());
+        return redirect('/mobile')->with('sukses', 'Data berhasil di Input!');
+    }
+
+    public function edit($id)
+    {
+        $mobile = MobileModel::find($id);
+        return view('Mobile Station.edit_mobile', ['mobile' => $mobile]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $mobile = MobileModel::find($id);
+        $mobile->update($request->all());
+        return redirect('/mobile')->with('sukses', 'Data berhasil di Update!');
+    }
+
+    public function delete($id)
+    {
+        $mobile = MobileModel::find($id);
+        $mobile->delete($mobile);
+        return redirect('/mobile')->with('sukses', 'Data berhasil dihapus!');
+    }
+}
