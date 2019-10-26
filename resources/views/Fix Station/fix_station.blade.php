b8ca1449ab2a013184f38e42aa73d1fb33bea03c
Muhammad Iqbal <iqbalgooner33@gmail.com>
2019-10-26 11:21:17 +0700

    Membuat folder Fix Station pada views

---
+++ resources/views/Fix Station/fix_station.blade.php
@@ -0,0 +1,124 @@
+@extends('master')
+ 
+@section('content')
+
+{{-- Notifikasi form validasi --}}
+    @if ($errors->has('file'))
+        <span class="invalid-feedback" role="alert">
+            <strong>{{$errors->first('file')}}</strong>
+        </span>
+    @endif
+
+    {{-- notifikasi sukses --}}
+	@if ($sukses = Session::get('sukses'))
+		<div class="alert alert-success alert-block">
+			<button type="button" class="close" data-dismiss="alert">×</button> 
+			<strong>{{ $sukses }}</strong>
+		</div>
+    @endif
+    
+    <button type="button" class="btn btn-primary my-3" data-toggle="modal" data-target="#importExcel">
+		 <i class="fas fa-file-excel"></i> Import Excel
+    </button>
+
+    <a href="/fix/export_excel" class="btn btn-success my-1" target="_blank">
+        <i class="fas fa-file-excel"></i> Export Excel
+    </a>
+    
+        <a href="/tampiladdfix" class="btn btn-primary">
+            <i class="fa fa-plus nav-icon"></i>
+        </a>
+
+<div class="card" style="border-top: 3px solid #9C5C22">
+        
+       <div class="card-header">
+            <h4>Master Fix Station</h4>
+        </div>
+
+    <div class="card-body">
+        <table class="table table-bordered" id="myTable">
+            <thead>
+                <tr>
+                    <th class="text-center">No</th>
+                    <th class="text-center">Nama Station</th>
+                    <th class="text-center">Alamat</th>
+                    <th class="text-center">Nama Lokasi</th>
+                    <th class="text-center">Koordinat GPS</th>
+                    <th class="text-center">Tank Number</th>
+                    <th class="text-center">Kapasitas Tangki</th>
+                    <th class="text-center">Fuelman Assignment</th>
+                    <th class="text-center">Pengisian Terakhir</th>
+                    <th class="text-center">Ending Stock Date</th>
+                    <th class="text-center">Ending Stock Quantity</th>
+                    <th class="text-center" width="8%">Action</th>
+                </tr>
+            </thead>
+            @php $i=1 @endphp
+            <tbody>
+                @php $i=1 @endphp
+                 @foreach($fix ?? '' as $s)
+                <tr>
+                    <td>{{$i++}}</td>
+                    <td>{{$s->name_station}}</td>
+                    <td>{{$s->address}}</td>
+                    <td>{{$s->nama_lokasi}}</td>
+                    <td>{{$s->koordinat_gps}}</td>
+                    <td>{{$s->tank_number}}</td>
+                    <td>{{$s->fuel_capacity}}</td>
+                    <td>{{$s->fuel_assignment}}</td>
+                    <td>{{$s->last_refuel}}</td>
+                    <td>{{$s->ending_stock_date}}</td>
+                    <td>{{$s->ending_stock_quantity}}</td>
+                    <td>
+                         <div class="btn-group">
+
+                            <!-- URL::to('/admin/category/detail.id='.$cate-id -->
+                             <a href="#" class="btn btn-info btn-sm" data-toggle="tooltip" data-placement="bottom" title="Info">
+                                <i class="fa fa-info-circle nav-icon"></i>
+                            </a>
+
+                            <a href="/fix/edit/{{$s->id}}" class="btn btn-warning  btn-sm" data-toggle="tootip" data-placement="bottom" title="Edit">
+                                <i class="fa fa-edit nav-icon"></i>
+                            </a>
+
+                            <a onClick="return confirm('Yakin ingin menghapus data?')" href="/fix/{{$s->id}}/delete" class="btn btn btn-danger btn-sm">
+                                <i class="fa fa-trash nav-icon"></i>
+                            </a>
+
+                        </div>
+                    </td>
+                    
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
+ <!-- Import Excel -->
+		<div class="modal fade" id="importExcel" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<form method="post" action="/equipment/import_excel" enctype="multipart/form-data">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h5 class="modal-title" id="exampleModalLabel">Import Excel</h5>
+						</div>
+						<div class="modal-body">
+ 
+							{{ csrf_field() }}
+ 
+							<label>Pilih file excel</label>
+							<div class="form-group">
+								<input type="file" name="file" required="required">
+							</div>
+ 
+						</div>
+						<div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+							<button type="submit" class="btn btn-primary">Import</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+@endsection
---
+++ resources/views/Fix Station/tambah_fixstation.blade.php
@@ -0,0 +1,93 @@
+@extends('master')
+
+@section('content')
+
+<div class="row">
+	<div class="col-md-12">
+		<form action="/fix/create" method="POST">
+
+			@csrf
+
+			<div class="card" style="border-top: 3px solid #9C5C22">
+				<div class="card-header">
+					<h3 class="card-title">Tambah Fix Station</h3>
+				</div>
+
+				<div class="card-body">
+
+					@if($errors->any())
+					<div class="alert alert-danger">
+						{{implode('', $errors->all(':message'))}}
+					</div>
+					@endif
+
+					<div class="form-group">
+						<label>Nama Station</label>
+						<input type="text " name="name_station" placeholder="" class="form-control" required autofocus>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">Alamat</label>
+                        <input type="text" name="address" placeholder="" class="form-control" required autofocus>
+                    </div>
+                    
+                    <div class="form-group">
+						<label>Nama Lokasi</label>
+						<input type="text" name="nama_lokasi" placeholder="" class="form-control" required autofocus>
+                    </div>
+                    
+                    <div class="form-group">
+						<label>Koordinat GPS</label>
+						<input type="text" name="koordinat_gps" placeholder="" class="form-control" required autofocus>
+                    </div>
+                    
+                    <div class="form-group">
+						<label>Tank Number</label>
+						<input type="text" name="tank_number" placeholder="" class="form-control" required autocomplete="">
+                    </div> 
+
+                    <div class="form-group">
+						<label>Kapasitas Tangki</label>
+						<input type="text" name="fuel_capacity" placeholder="" class="form-control" required autocomplete="">
+                    </div> 
+
+                    <div class="form-group">
+						<label>Fuel Assignment</label>
+						<input type="text" name="fuel_assignment" placeholder="" class="form-control" required autocomplete="">
+                    </div> 
+
+                    <div class="form-group">
+						<label>Pengisian Terakhir</label>
+						<input type="text" name="last_refuel" placeholder="" class="form-control" required autocomplete="">
+                    </div> 
+
+                    <div class="form-group">
+						<label>Ending Stock Date</label>
+						<input type="text" name="ending_stock_date" placeholder="" class="form-control" required autocomplete="">
+                    </div> 
+
+                    <div class="form-group">
+						<label>Ending Stock Quantity</label>
+						<input type="text" name="ending_stock_quantity" placeholder="" class="form-control" required autocomplete="">
+                    </div> 
+
+				</div>
+
+				<div class="card-footer">
+
+					<a href="/fix" class="btn btn-default">Back</a>
+                    &nbsp;&nbsp;
+					<input type="submit" value="Add Data" class="pull-right btn btn-primary">
+
+				</div>
+
+			</div>
+
+		</form>
+
+	</div>
+</div>
+
+
+
+@endsection
