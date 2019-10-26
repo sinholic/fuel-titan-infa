d934aef65fc3da8f8deddd568be794224ac358fb
Muhammad Iqbal <iqbalgooner33@gmail.com>
2019-10-26 13:49:13 +0700

    Membuat API EquipmentController.php

---
+++ app/Http/Controllers/API/EquipmentController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use \App\EquipmentModel;
+
+class EquipmentController extends Controller
+{
+    public function lists()
+    {
+        $equipments = EquipmentModel::all();
+        return response()->json(['data' => $equipments]);
+    }
+
+    public function tambah()
+    {
+        return view('Equipment.tambah_equipment');
+    }
+
+    public function create(Request $request)
+    {
+        $equipment = EquipmentModel::create($request->all());
+        return response()->json(['data' => $equipment]);
+    }
+
+    public function edit($id)
+    {
+        $equipment = EquipmentModel::find($id);
+        return view('Equipment.edit_equipment', ['equipment' => $equipment]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $equipment = EquipmentModel::find($id);
+        $equipment->update($request->all());
+        return redirect('/equipment')->with('sukses', 'Data berhasil di Update!');
+    }
+}
