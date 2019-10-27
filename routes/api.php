e419be7396b36e7ce3f283c49a8c9686ad7f3d49
Muhammad Iqbal <iqbalgooner33@gmail.com>
2019-10-27 13:43:35 +0700

    Membuat route pada api.php

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,33 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::group(['middleware' => 'auth:api'], function () {
     Route::post('details', 'API\UserController@details');
 });
+Route::group(['prefix' => 'equipment'], function () {
+    Route::get('lists', 'API\EquipmentController@lists');
+    Route::post('create', 'API\EquipmentController@create');
+});
+
+//Mobile Station
+Route::group(['prefix' => 'mobile'], function () {
+    Route::get('/', 'API\MobileController@mobile');
+    Route::post('/create', 'API\MobileController@create');
+    Route::post('/update/{id}', 'API\MobileController@update');
+    Route::get('/{id}/delete', 'API\MobileController@delete');
+});
+
+//Owner
+Route::group(['prefix' => 'owner'], function () {
+    Route::get('/', 'API\OwnerController@lists');
+    Route::post('/create', 'API\OwnerController@create');
+    Route::post('/update/{id}', 'API\OwnerController@update');
+    Route::get('/{id}/delete', 'API\OwnerController@delete');
+});
+
+//Organization
+Route::group(['prefix' => 'organization'], function () {
+    Route::get('/', 'API\OrganizationController@lists');
+    Route::post('/create', 'API\OrganizationController@create');
+    Route::post('/update/{id}', 'API\Organization@update');
+});
 
 Route::get('siswa', 'SiswaController@index');
 Route::post('siswa', 'SiswaController@create');
