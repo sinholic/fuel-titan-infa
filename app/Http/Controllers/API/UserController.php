135be878a4d3ccdb1ddb184f90c742ba25f0ab63
Mahar Prasetio <mahar@misua.id>
2019-10-28 08:36:01 +0700

    add imei to validation and migration

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
 
     public function login()
     {
-        if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
+        if (Auth::attempt(['email' => request('email'), 'password' => request('password'), 'imei' => request('imei')])) {
             $user = Auth::user();
             $success['token'] = $user->createToken('nApp')->accessToken;
             return response()->json(['success' => $success], $this->successStatus);
