4382177aaec3d18e9003c416c2b82eb81ae4d506
Muhammad Iqbal <iqbalgooner33@gmail.com>
2019-11-01 01:21:33 +0700

    Membuat migration reloading.php

---
+++ database/migrations/2019_10_31_125139_create_reloading_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReloadingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reloading', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('no_po');
+            $table->string('unit_mobile_station');
+            $table->string('driver_mobile_statis');
+            $table->integer('qty_solar');
+            $table->integer('odometer');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reloading');
+    }
+}
