5e8cab525d1b277dc694e4e25adbc94569770e21
Muhammad Iqbal <iqbalgooner33@gmail.com>
2019-10-31 23:55:42 +0700

    Mengubah tampilan FuelmanModel.php

--- app/FuelmanModel.php
+++ app/FuelmanModel.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class FuelmanModel extends Model
 {
     protected $table = "fuelman";
-    protected $fillable = ['nik', 'name', 'location_assignment', 'password_login', 'password_sync', 'imei'];
+    protected $fillable = ['id', 'nik', 'name', 'location_assignment', 'password_login', 'password_sync', 'imei'];
     public $timestamps = false;
 }
