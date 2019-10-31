58e37f1f3e36ca8b28c7e7300c500b8ff2b3882d
Mahar Prasetio <sin.teta0@gmail.com>
2019-11-01 00:00:08 +0700

    fix migrations

--- app/StationModel.php
+++ app/StationModel.php
@@ -9,5 +9,4 @@ class StationModel extends Model
     protected $guarded = [];
     protected $table = "station";
     protected $fillable = ['nama_lokasi', 'address', 'koordinat_gps', 'tank_number', 'fuel_capacity', 'fuel_assignment', 'last_refuel', 'ending_stock_date', 'ending_stock_quantity'];
-    public $timestamps = false;
 }
