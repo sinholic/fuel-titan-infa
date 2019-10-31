10b903c0992a280a8689603bad7d6c1f338b4698
Muhammad Iqbal <iqbalgooner33@gmail.com>
2019-11-01 01:20:26 +0700

    Menambah migration organization

---
+++ database/migrations/2019_10_31_172354_create_organization_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrganizationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organization', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nik');
+            $table->string('name');
+            $table->string('password');
+            $table->string('organization');
+            $table->string('owner');
+            $table->string('password_sync');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organization');
+    }
+}
