99351c3e85af5556df7867380dc67503f25799ba
Muhammad Iqbal <iqbalgooner33@gmail.com>
2019-10-31 12:59:12 +0700

    Mengubah tampilan print_voucher.blade.php

--- resources/views/Voucher/print_voucher.blade.php
+++ resources/views/Voucher/print_voucher.blade.php
@@ -7,19 +7,26 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
     <title>Print Voucher</title>
+    <style type="text/css">
+        .table {
+            height: 4cm;
+            width: 4cm;
+        }
+        .jumlah {
+            font-size: 30px;
+        }
+    </style>
   </head>
   <body>
-<a href="#" class="btn btn-primary" onclick="window.print();">Print</a>
+<a href="#" class="btn btn-primary" style="float: right"; onclick="window.print();">Print</a>
 
 <div class="container">
     <div class="row">
         <div class="col">
-            <table border="3">
+            <table border="3" class="table">
                 <tr>
-                    <th rowspan="2" colspan="2" class="text-center"><b>UNIT IDENTITY CARD</b></th>
-                    <th colspan="2"></td>
+                    <th rowspan="2" colspan="2" class="text-center"><b>Visual Voucher</b></th>
                 </tr>
                 <tbody>
                 @foreach($voucher  as $s)
@@ -30,8 +37,8 @@
                         Expired: $s->expired_date"
                     @endphp
                 <tr>
-                    <td>{!! QrCode::size(200)->generate($string); !!}</td>
-                    <td>Jumlah:  {{$s->qty}} Ltr <br> Owner: {{$s->owner}} <br> Kadaluarsa: {{date('d-M-Y', strtotime($s->expired_date))}}</td>
+                    <td>{!! QrCode::size(200)->generate($string); !!}
+                    <b class="jumlah">{{$s->qty}}L</b></<b> <br> Owner: {{$s->owner}} <br> Kadaluarsa: {{date('d M Y', strtotime($s->expired_date))}}</td>
                 </tr>
                  @endforeach
                 </tbody>
