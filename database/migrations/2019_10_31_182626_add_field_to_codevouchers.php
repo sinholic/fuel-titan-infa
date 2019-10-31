0bd40d113a8237a89592eda53e86739a7d442dde
Mahar Prasetio <sin.teta0@gmail.com>
2019-11-01 01:52:20 +0700

    fix migrations

---
+++ database/migrations/2019_10_31_182626_add_field_to_codevouchers.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFieldToCodevouchers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('codevouchers', function (Blueprint $table) {
+            $table->bigInteger('voucher_id');
+            $table->string('code_number');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('codevouchers', function (Blueprint $table) {
+            $table->dropColumn('voucher_id', 'code_number');
+            $table->dropSoftDeletes();
+        });
+    }
+}
