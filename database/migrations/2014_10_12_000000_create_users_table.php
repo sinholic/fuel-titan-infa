c94f7b9e7d0c3f8b0eb612c9a08e30385eb6d43a
Muhammad Iqbal <iqbalgooner33@gmail.com>
2019-10-31 23:12:01 +0700

    Membuat migrations users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,12 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('imei');
+            $table->string('syncpassword');
+            $table->bigInteger('status_id');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
