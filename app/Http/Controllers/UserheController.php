bc3eaf76f6a5e32ec4c0faaef79f1bc92f393246
Muhammad Iqbal <iqbalgooner33@gmail.com>
2019-11-05 14:08:04 +0700

    Menambah UserheController.php untuk timesheet running hour machine

--- app/Http/Controllers/UserheController.php
+++ app/Http/Controllers/UserheController.php
@@ -23,4 +23,24 @@ class UserheController extends Controller
         UserheModel::create($request->all());
         return redirect('/userhe')->with('sukses', 'Data berhasil diinput!');
     }
+
+    public function edit($id)
+    {
+        $userhe = UserheModel::find($id);
+        return view('User HE.edit_userhe', ['userhe' => $userhe]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $userhe = UserheModel::find($id);
+        $userhe->update($request->all());
+        return redirect('/userhe')->with('sukses', 'Data berhasil di Update');
+    }
+
+    public function delete($id)
+    {
+        $userhe = UserheModel::find($id);
+        $userhe->delete($userhe);
+        return redirect('/userhe')->with('sukses', 'Data berhasil dihapus');
+    }
 }
