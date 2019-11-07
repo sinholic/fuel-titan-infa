4dae237fcbf3be9d04d911c1801d56ed898eda7e
Muhammad Iqbal <iqbalgooner33@gmail.com>
2019-11-07 14:29:48 +0700

    Membuat PenerimaanModel.php

--- app/PenerimaanModel.php
+++ app/PenerimaanModel.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PenerimaanModel extends Model
 {
-    //
+    use SoftDeletes;
+    protected $table = "penerimaan";
+    protected $fillable = ['remark', 'supplier', 'no_po', 'qty', 'no_tangki'];
 }
