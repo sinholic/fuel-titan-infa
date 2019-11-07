717ade5f30e0ea9580d076a5865b9255173f75de
Muhammad Iqbal <iqbalgooner33@gmail.com>
2019-11-07 14:28:32 +0700

    Menambah Equipmentcategory.php

--- app/Equipmentcategory.php
+++ app/Equipmentcategory.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Equipmentcategory extends Model
 {
-    //
+    use SoftDeletes;
+    protected $fillable = ['nama', 'inisial'];
 }
