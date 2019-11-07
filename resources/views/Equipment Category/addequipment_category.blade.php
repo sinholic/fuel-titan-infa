254d5d130bd616527a00f596d5dfe984a20144f1
Muhammad Iqbal <iqbalgooner33@gmail.com>
2019-11-07 14:37:59 +0700

    Menambah Folder Equipment Category

---
+++ resources/views/Equipment Category/addequipment_category.blade.php
@@ -0,0 +1,53 @@
+@extends('master')
+
+@section('content')
+
+<div class="row">
+	<div class="col-md-12">
+		<form action="/equipment_category/create" method="POST">
+
+			@csrf
+
+			<div class="card" style="border-top: 3px solid #9C5C22">
+				<div class="card-header">
+					<h3 class="card-title">Tambah Equipment Category</h3>
+				</div>
+
+				<div class="card-body">
+
+					@if($errors->any())
+					<div class="alert alert-danger">
+						{{implode('', $errors->all(':message'))}}
+					</div>
+					@endif
+
+					<div class="form-group">
+						<label>Nama Equipment</label>
+						<input type="text " name="nama" placeholder="" class="form-control" required autofocus>
+                    </div>
+
+                    <div class="form-group">
+						<label>Inisial</label>
+						<input type="text " name="inisial" placeholder="" class="form-control" required autofocus>
+                    </div>        
+
+				</div>
+
+				<div class="card-footer">
+
+					<a href="/equipment_category" class="btn btn-default">Back</a>
+                    &nbsp;&nbsp;
+					<input type="submit" value="Add Data" class="pull-right btn btn-primary">
+
+				</div>
+
+			</div>
+
+		</form>
+
+	</div>
+</div>
+
+
+
+@endsection
---
+++ resources/views/Equipment Category/edit_equipment_category.blade.php
@@ -0,0 +1,53 @@
+@extends('master')
+
+@section('content')
+
+<div class="row">
+    <div class="col-md-12">
+        <form action="/equipment_category/update/{{$equipment_category->id}}" method="POST">
+
+            @csrf
+           
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Edit Equipment Category</h3>
+                </div>
+
+                <div class="card-body">
+
+                    @if($errors->any())
+                    <div class="alert alert-danger">
+                        {{implode('', $errors->all(':message'))}}
+                    </div>
+                    @endif
+
+                    <div class="form-group">
+                        <label>Nama</label>
+                        <input type="text" name="nama" value="{{$equipment_category->nama}}" class="form-control" required autofocus>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">Inisial</label>
+                        <input type="text" name="inisial" value="{{$equipment_category->inisial}}" class="form-control" required autofocus>
+                    </div>
+
+
+                </div>
+
+                <div class="card-footer">
+
+                    <a href="/equipment_category" class="btn btn-default">Back</a>
+                    <input type="submit" value="Update Data" class="pull-right btn btn-warning">
+
+                </div>
+
+            </div>
+
+        </form>
+
+    </div>
+</div>
+
+
+
+@endsection('content')
