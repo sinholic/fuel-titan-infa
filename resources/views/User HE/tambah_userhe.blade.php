e339b9beb54e69d7be4b1aab606568abbee5407d
Muhammad Iqbal <iqbalgooner33@gmail.com>
2019-11-11 16:41:52 +0700

    Membuat tampilan tambah_userhe.blade.php

--- resources/views/User HE/tambah_userhe.blade.php
+++ resources/views/User HE/tambah_userhe.blade.php
@@ -34,73 +34,103 @@
                     <div class="form-group">
 						<label>Tanggal Operasi</label>
 						<input type="date" name="tanggal_operasi" placeholder="" class="form-control" required autofocus>
-                    </div>
-                    
-                    <div class="form-group">
-						<label>Nama Unit</label>
-						<input type="text" name="nama_unit" placeholder="" class="form-control" required autofocus>
-                    </div>
-                    
-                    <div class="form-group">
-						<label>Penyewa</label>
-						<input type="text" name="penyewa" placeholder="" class="form-control" required autocomplete="">
 					</div>
 					
 					<div class="form-group">
-						<label>HM Awal</label>
-						<input type="number" name="hm_awal" placeholder="" class="form-control" required autocomplete="">
+						<label>Area Kerja</label>
+						<input type="text" name="nama_unit" placeholder="" class="form-control" required autofocus>
+					</div>
+					
+					<div class="form-group">
+						<label>Owner</label>
+						<input type="text" name="penyewa" placeholder="" class="form-control" required autofocus>
                     </div>
+					
+					<div class="row">
+						<div class="col-md-6">
+							<div class="form-group">
+								<label>HM Awal</label>
+								<input type="number" name="hm_awal" placeholder="" class="form-control" required autocomplete="">
+							</div>
+						</div>
+					
+						<div class="col-md-6">
+							<div class="form-group">
+								<label>KM Awal</label>
+								<input type="number" name="km_awal" placeholder="" class="form-control" required autocomplete="">
+							</div>
+						</div>
+					</div>
 
-                    <div class="form-group">
-						<label>HM Akhir</label>
-						<input type="number" name="hm_akhir" placeholder="" class="form-control" required autocomplete="">
-                    </div>
+					<div class="row">
+						<div class="col-md-6">
+                            <div class="form-group">
+                                <label>HM Akhir</label>
+                                <input type="number" name="hm_akhir" placeholder="" class="form-control" required autocomplete="">
+                            </div>
+						</div>
+						
+						<div class="col-md-6">
+							<div class="form-group">
+								<label>KM Akhir</label>
+								<input type="number" name="km_akhir" placeholder="" class="form-control" required autocomplete="">
+                    		</div>
+						</div>
+					</div>
 
-                    <div class="form-group">
-						<label>Total Jam</label>
-						<input type="number" name="total_jam" placeholder="" class="form-control" required autocomplete="">
-                    </div>
+					<div class="row">
+						<div class="col-md-6">
+							<div class="form-group">
+								<label>Total Jam</label>
+								<input type="number" name="total_jam" placeholder="" class="form-control" required autocomplete="">
+							</div>
+						</div>
+
+						<div class="col-md-6">
+							<div class="form-group">
+								<label>Total KM</label>
+								<input type="number" name="km_total" placeholder="" class="form-control" required autocomplete="">
+                    		</div>
+						</div>
+					</div>
 
                     <div class="form-group">
 						<label>Job Order</label>
-						<input type="text" name="job_order" placeholder="" class="form-control" required autocomplete="">
+						<textarea name="job_order" class="form-control" rows="3" required autocomplete=""></textarea>
                     </div>
 
-                    <div class="form-group">
-						<label>BBM</label>
-						<input type="text" name="bbm" placeholder="" class="form-control" required autocomplete="">
-                    </div>
+					<div class="row">
+						<div class="col-md-6">
+							<div class="form-group">
+								<label>BBM</label>
+								<input type="text" name="bbm" placeholder="" class="form-control" required autocomplete="">
+							</div>
+						</div>
 
-                    <div class="form-group">
-						<label>Operator</label>
-						<input type="text" name="operator" placeholder="" class="form-control" required autocomplete="">
-                    </div>
-
-                    <div class="form-group">
-						<label>Helper</label>
-						<input type="text" name="helper" placeholder="" class="form-control" required autocomplete="">
-                    </div>
+						<div class="col-md-6">
+							<div class="form-group">
+								<label>Operator</label>
+								<input type="text" name="operator" placeholder="" class="form-control" required autocomplete="">
+                    		</div>
+						</div>
+					</div>
                     
-                    <div class="form-group">
-						<label>Pengawas</label>
-						<input type="text" name="pengawas" placeholder="" class="form-control" required autocomplete="">
-                    </div>
+					<div class="row">
+						<div class="col-md-6">
+							<div class="form-group">
+								<label>Helper</label>
+								<input type="text" name="helper" placeholder="" class="form-control" required autocomplete="">
+                    		</div>
+						</div>
 
-                    <div class="form-group">
-						<label>KM Awal</label>
-						<input type="number" name="km_awal" placeholder="" class="form-control" required autocomplete="">
-                    </div>
-
-                    <div class="form-group">
-						<label>KM Akhir</label>
-						<input type="number" name="km_akhir" placeholder="" class="form-control" required autocomplete="">
-                    </div>
-
-                    <div class="form-group">
-						<label>KM Total</label>
-						<input type="number" name="km_total" placeholder="" class="form-control" required autocomplete="">
-                    </div>
-
+						<div class="col-md-6">
+							<div class="form-group">
+								<label>Pengawas</label>
+								<input type="text" name="pengawas" placeholder="" class="form-control" required autocomplete="">
+                    		</div>
+						</div>
+					</div>
+					
 				</div>
 
 				<div class="card-footer">
