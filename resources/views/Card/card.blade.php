e70301c06a540b1bf4d7adbde631a5a0a6543f85
Muhammad Iqbal <iqbalgooner33@gmail.com>
2019-11-11 16:38:07 +0700

    Menambahkan tampilan Card Number

---
+++ resources/views/Card/card.blade.php
@@ -0,0 +1,108 @@
+@extends('master')
+ 
+@section('content')
+
+{{-- Notifikasi form validasi --}}
+    @if ($errors->has('file'))
+        <span class="invalid-feedback" role="alert">
+            <strong>{{$errors->first('file')}}</strong>
+        </span>
+    @endif
+
+    {{-- notifikasi sukses --}}
+	@if ($sukses = Session::get('sukses'))
+		<div class="alert alert-success alert-block">
+			<button type="button" class="close" data-dismiss="alert">×</button> 
+			<strong>{{ $sukses }}</strong>
+		</div>
+    @endif
+    
+    <button type="button" class="btn btn-primary my-3" data-toggle="modal" data-target="#importExcel">
+		 <i class="fas fa-file-excel"></i> Import Excel
+    </button>
+
+    <a href="/card/export_excel" class="btn btn-success my-1" target="_blank">
+        <i class="fas fa-file-excel"></i> Export Excel
+    </a>
+    
+        <a href="/tambahcard" class="btn btn-primary">
+            <i class="fa fa-plus nav-icon"></i>
+        </a>
+
+<div class="card" style="border-top: 3px solid #9C5C22">
+        
+       <div class="card-header">
+            <h4>Card Number</h4>
+        </div>
+
+    <div class="card-body">
+        <table class="table table-striped table table-bordered" id="myTable">
+            <thead style="background-color: #9C5C22">
+                <tr>
+                    <th class="text-center">No</th>
+                    <th class="text-center">Card Number</th>
+                    <th class="text-center">Equipment ID</th>
+                    <th class="text-center" width="8%">Action</th>
+                </tr>
+            </thead>
+            @php $i=1 @endphp
+            <tbody>
+                @php $i=1 @endphp
+                 @foreach($card ?? '' as $s)
+                <tr>
+                    <td>{{$i++}}</td>
+                    <td>{{$s->cardnumber}}</td>
+                    <td>{{$s->equipment_id}}</td>
+                    <td>
+                         <div class="btn-group">
+
+                            <!-- URL::to('/admin/category/detail.id='.$cate-id -->
+                             <a href="#" class="btn btn-info btn-sm" data-toggle="tooltip" data-placement="bottom" title="Info">
+                                <i class="fa fa-info-circle nav-icon"></i>
+                            </a>
+
+                            <a href="/card/edit/{{$s->id}}" class="btn btn-warning  btn-sm" data-toggle="tootip" data-placement="bottom" title="Edit">
+                                <i class="fa fa-edit nav-icon"></i>
+                            </a>
+
+                            <a onClick="return confirm('Yakin ingin menghapus data?')" href="/card/{{$s->id}}/delete" class="btn btn btn-danger btn-sm">
+                                <i class="fa fa-trash nav-icon"></i>
+                            </a>
+
+                        </div>
+                    </td>
+                    
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
+ <!-- Import Excel -->
+		<div class="modal fade" id="importExcel" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<form method="post" action="/card/import_excel" enctype="multipart/form-data">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h5 class="modal-title" id="exampleModalLabel">Import Excel</h5>
+						</div>
+						<div class="modal-body">
+ 
+							{{ csrf_field() }}
+ 
+							<label>Pilih file excel</label>
+							<div class="form-group">
+								<input type="file" name="file" required="required">
+							</div>
+ 
+						</div>
+						<div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+							<button type="submit" class="btn btn-primary">Import</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+@endsection
