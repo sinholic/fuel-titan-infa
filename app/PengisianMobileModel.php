49f7e6f1aa09c452ff4b6fd4844c36a70bf51162
Muhammad Iqbal <iqbalgooner33@gmail.com>
2019-11-13 14:09:07 +0700

    Membuat relasi PengisianMobileModel.php

--- app/PengisianMobileModel.php
+++ app/PengisianMobileModel.php
@@ -9,7 +9,7 @@ class PengisianMobileModel extends Model
     protected $table = "pengisian_mobile";
     protected $fillable = ['unit_equipment', 'id_driver', 'qty_solar', 'odometer', 'remark'];
 
-    public function mobile()
+    public function qtysolar()
     {
         return $this->belongsTo('App\QtySolarModel', 'qty_solar', 'id');
     }
