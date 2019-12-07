4a3052fc79edc39b1e160f19e75950c9bb57390b
Asadullah Al Hamasy <asadhamasy@gmail.com>
2019-12-07 20:47:45 +0700

    change routes api

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::post('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
 
 Route::group(['middleware' => 'auth:api'], function () {
-    Route::get('details', 'API\UserController@details');
+    Route::post('details', 'API\UserController@details');
 
     Route::group(['prefix' => 'equipment'], function () {
         Route::get('lists', 'API\EquipmentController@lists');
